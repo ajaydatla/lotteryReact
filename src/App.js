99e77bdd87a9afccf192f9240e747f124327e432
ajaydatla <ajaydatlalbn06@gmail.com>
2018-08-20 17:05:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+
+  state = {
+    manager:'',
+    players:[],
+    balance: '',
+    value:'',
+    message:''
+  }
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({manager, players, balance});
+  }
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'waiting on transaction success...'});
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({message: 'You have been entered..'});
+
+  }
+
+  onClick = async() => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.pickWinner().send({
+      from : accounts[0]
+    });
+
+    this.setState({message: 'Winner has been picked'});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>
+          Lottery Contract
+        </h2>
+        <p>
+          This contract is managed by: {this.state.manager}. </p>
+          <p>There are currently {this.state.players.length} players in the competetion, competing for {web3.utils.fromWei(this.state.balance, 'ether')} ether.</p>
+       <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <label>Enter ethers:</label>
+          <input type='number' value={this.state.value} 
+          onChange={event => this.setState({value: event.target.value})} />
+          <button>Enter</button>
+        </form>
+          <h3>
+            Ready to pick up a winner?
+          </h3>
+          <button onClick={this.onClick}>Pick a winner</button>
+        <hr />
+        <h2>{this.state.message}</h2>
+
+      </div>
+    );
+  }
+}
+
+export default App;
